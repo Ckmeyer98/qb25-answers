f47c1cbf06873a3e85a00862393b69c0296cbaaa
Clayton Meyer <Ckmeyer98@gmail.com>
2025-09-18 21:27:23 -0400

    Started Exercise 2

---
+++ week1/exercise2.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+header <- c( "chr", "start", "end", "count")
+
+df_kc_19 <- read_tsv("/Users/cmdb/qb25-answers/week1/hg19-kc-count.bed", col_names=header)
+
+df_kc_16 <- read_tsv("/Users/cmdb/qb25-answers/week1/hg16-kc-count.bed", col_names=header)
+
+
+ggplot(bind_rows( hg19=dfA, hg16=dfB, .id="assembly" ), aes(x = start, y = count, color = assembly)) +
+  geom_line() +
+  facet_wrap(chr ~ ., scales = 'free') +
+  labs(
+    x = "Position",
+    y = "Gene Density",
+    title = "Gene Density Across Chromosomes"
+  )
