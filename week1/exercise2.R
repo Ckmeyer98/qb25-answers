eaf7de66732a8b12b31ce1dccab6cd3eed52485e
Clayton Meyer <Ckmeyer98@gmail.com>
2025-09-29 18:15:42 -0400

    Added R script for generating gene density plot

--- week1/exercise2.R
+++ week1/exercise2.R
@@ -7,7 +7,7 @@ df_kc_19 <- read_tsv("/Users/cmdb/qb25-answers/week1/hg19-kc-count.bed", col_nam
 df_kc_16 <- read_tsv("/Users/cmdb/qb25-answers/week1/hg16-kc-count.bed", col_names=header)
 
 
-ggplot(bind_rows( hg19=dfA, hg16=dfB, .id="assembly" ), aes(x = start, y = count, color = assembly)) +
+ggplot(bind_rows( hg19=df_kc_19, hg16=df_kc_16, .id="assembly" ), aes(x = start, y = count, color = assembly)) +
   geom_line() +
   facet_wrap(chr ~ ., scales = 'free') +
   labs(
@@ -15,3 +15,4 @@ ggplot(bind_rows( hg19=dfA, hg16=dfB, .id="assembly" ), aes(x = start, y = count
     y = "Gene Density",
     title = "Gene Density Across Chromosomes"
   )
+
